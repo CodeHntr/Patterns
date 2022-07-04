8e3512612d46f6c124c1454016a8241d5c15f98b
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-04 18:06:16 +0300

    add protected properties and magic function construct

--- src/Facade/Facade.php
+++ src/Facade/Facade.php
@@ -4,5 +4,16 @@ namespace Patterns\Facade;
 
 class Facade
 {
+    protected $subsystem1;
+
+    protected $subsystem2;
+
+    public function __construct(
+        Subsystem1 $subsystem1 = null,
+        Subsystem2 $subsystem2 = null
+    ) {
+        $this->subsystem1 = $subsystem1 ?: new Subsystem1();
+        $this->subsystem2 = $subsystem2 ?: new Subsystem2();
+    }
 
 }
