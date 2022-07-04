f718de5184d6710e6032be53dd0094b5ea00a9cc
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-04 18:09:12 +0300

    Add function realize in Facade class

--- src/Facade/Facade.php
+++ src/Facade/Facade.php
@@ -16,4 +16,15 @@ class Facade
         $this->subsystem2 = $subsystem2 ?: new Subsystem2();
     }
 
+    public function reaize(): string
+    {
+        $result = "Facade initializes subsystems:\n";
+        $result .= $this->subsystem1->operation1();
+        $result .= $this->subsystem2->operation1();
+        $result .= "Facade orders subsystems to perform the action:\n";
+        $result .= $this->subsystem1->operationN();
+        $result .= $this->subsystem2->operationZ();
+
+        return $result;
+    }
 }
