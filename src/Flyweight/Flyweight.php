8c284d867c1bb13c7aaa75e80babbb054d170811
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-07 14:12:01 +0300

    Add function acceptData

--- src/Flyweight/Flyweight.php
+++ src/Flyweight/Flyweight.php
@@ -10,5 +10,11 @@ class Flyweight
     {
         $this->sharedState = $sharedState;
     }
-
+    public function acceptData(array $uniqueState): void
+    {
+        $common = json_encode($this->sharedState);
+        $unique = json_encode($uniqueState);
+        echo "Flyweight: Відображення спільних ($common) і унікальних ($unique)<br />";
+    }
+}
 
