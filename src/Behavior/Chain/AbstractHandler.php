7015a390736b6ef3aa4073a16148fd76fed1eab0
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-18 12:48:19 +0300

    add to abstract Class second function handle()

--- src/Behavior/Chain/AbstractHandler.php
+++ src/Behavior/Chain/AbstractHandler.php
@@ -11,4 +11,12 @@ abstract class AbstractHandler implements Handler
         $this->nextHandler = $handler;
         return $handler;
     }
+
+    public function handle(string $request): ?string
+    {
+        if ($this->nextHandler) {
+            return $this->nextHandler->handle($request);
+        }
+        return null;
+    }
 }
