7500a8a2fe47d8021cf2e20523e39bf1130e7a88
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-19 12:57:17 +0300

    Add first function do Something

--- src/Behavior/Command/Receiver.php
+++ src/Behavior/Command/Receiver.php
@@ -4,5 +4,10 @@ namespace Patterns\Behavior\Command;
 
 class Receiver
 {
+    public function doSomething(string $a): void
+    {
+        echo "Receiver: Працює над (" . $a . ") <br />";
+    }
+
 
 }
