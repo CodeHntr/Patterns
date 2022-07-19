a41e71f04f4722a608bac9de792a70d06c2feb39
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-19 17:10:56 +0300

    Create Alphabetical iterator class and add property type of  WordsCollection

---
+++ src/Behavior/Iterator/AlphabeticalOrderIterator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Patterns\Behavior\Iterator;
+
+
+class AlphabeticalOrderIterator implements \Iterator
+{
+    /**
+     * @var WordsCollection
+     */
+    private WordsCollection $collection;
+
+}
