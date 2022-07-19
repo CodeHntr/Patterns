d018ae196c93618daa31a6392a7438fe09cacd19
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-20 00:07:21 +0300

    add getCherry function

--- src/Behavior/Mediator/Component1.php
+++ src/Behavior/Mediator/Component1.php
@@ -4,5 +4,10 @@ namespace Patterns\Behavior\Mediator;
 
 class Component1 extends BaseComponent
 {
+    public function getCherry(): void
+    {
+        echo "Об'єкт Вишня<br />";
+        $this->mediator->notify($this, "Вишня");
+    }
 
 }
