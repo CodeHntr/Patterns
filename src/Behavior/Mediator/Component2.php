cf321b320daf901643076e5734fdbfe597db1321
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-20 00:10:09 +0300

    add getBee function

--- src/Behavior/Mediator/Component2.php
+++ src/Behavior/Mediator/Component2.php
@@ -4,5 +4,10 @@ namespace Patterns\Behavior\Mediator;
 
 class Component2 extends BaseComponent
 {
+    public function getBee(): void
+    {
+        echo "Бджола<br />";
+        $this->mediator->notify($this, "Бджола");
+    }
 
 }
