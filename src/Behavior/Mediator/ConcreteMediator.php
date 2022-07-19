71b31554a0b74d984bd1d5741676d457f70f6f8e
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-20 00:04:27 +0300

    add to ConcreteMediator construct

--- src/Behavior/Mediator/ConcreteMediator.php
+++ src/Behavior/Mediator/ConcreteMediator.php
@@ -8,4 +8,12 @@ class ConcreteMediator implements Mediator
 
     private Component2 $component2;
 
+    public function __construct(Component1 $c1, Component2 $c2)
+    {
+        $this->component1 = $c1;
+        $this->component1->setMediator($this);
+        $this->component2 = $c2;
+        $this->component2->setMediator($this);
+    }
+
 }
