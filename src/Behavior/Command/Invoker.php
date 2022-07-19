f7f574dbb8ba0497eb025471fd2d5fec6c28bd93
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-19 15:57:40 +0300

    add executive function

--- src/Behavior/Command/Invoker.php
+++ src/Behavior/Command/Invoker.php
@@ -20,4 +20,19 @@ class Invoker
         $this->onFinish = $command;
     }
 
+    public function doSomethingImportant(): void
+    {
+        echo "Invoker: Хто виконає роботу перед початком моєї роботи?<br />";
+        if ($this->onStart instanceof Command) {
+            $this->onStart->execute();
+        }
+
+        echo "Invoker: ...робить дещо важливе...<br />";
+
+        echo "Invoker: Можливо хтось бажає зробити щось перед тим, як я завершу?<br />";
+
+        if ($this->onFinish instanceof Command) {
+            $this->onFinish->execute();
+        }
+    }
 }
