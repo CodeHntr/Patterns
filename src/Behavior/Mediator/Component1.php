e2b4480aee03a3f3ac0fb0f3e5a76226a8fcbbad
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-20 00:08:30 +0300

    add function getFact

--- src/Behavior/Mediator/Component1.php
+++ src/Behavior/Mediator/Component1.php
@@ -10,4 +10,9 @@ class Component1 extends BaseComponent
         $this->mediator->notify($this, "Вишня");
     }
 
+    public function getFact(): void
+    {
+        echo "З квіток вишні медоносні бджоли збирають щедрий «урожай» нектару і пилку<br />";
+        $this->mediator->notify($this, "B");
+    }
 }
