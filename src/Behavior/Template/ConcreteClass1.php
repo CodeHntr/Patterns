4ccd7b85d246cde1f621e68817cc6fb94a7489ee
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-21 01:13:59 +0300

    create First concreteclass1 with first function

---
+++ src/Behavior/Template/ConcreteClass1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Patterns\Behavior\Template;
+
+class ConcreteClass1 extends AbstractClass
+{
+    protected function requiredOperations1(): void
+    {
+      echo "ConcreteClass1 каже: Реалізована операція1<br />";
+    }
+
+
+}
