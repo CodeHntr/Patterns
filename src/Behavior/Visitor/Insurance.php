a4f4de369a25cf2d86e2510f7bafaa4cd5838bbc
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-21 01:55:02 +0300

    add to Insurance function visitHouse

--- src/Behavior/Visitor/Insurance.php
+++ src/Behavior/Visitor/Insurance.php
@@ -4,5 +4,9 @@ namespace Patterns\Behavior\Visitor;
 
 class Insurance implements Visitor
 {
+    public function visitHouse(House $element): void
+    {
+        echo $element->offerForHouse() . " Insurance company<br />";
+    }
 
 }
