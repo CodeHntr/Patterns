f984ef5def720b49a405925c537b9881e8bb52bf
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-21 01:14:41 +0300

    add second  function

--- src/Behavior/Template/ConcreteClass1.php
+++ src/Behavior/Template/ConcreteClass1.php
@@ -9,5 +9,8 @@ class ConcreteClass1 extends AbstractClass
       echo "ConcreteClass1 каже: Реалізована операція1<br />";
     }
 
-
+    protected function requiredOperation2(): void
+    {
+        echo "ConcreteClass1 каже: реалізована операція2<br />";
+    }
 }
