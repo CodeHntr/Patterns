a7abf30f5a773c73fc9a947b0d50960e2b3ab90e
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-21 01:47:31 +0300

    Add to House function accept

--- src/Behavior/Visitor/House.php
+++ src/Behavior/Visitor/House.php
@@ -4,5 +4,9 @@ namespace Patterns\Behavior\Visitor;
 
 class House implements Component
 {
+    public function accept(Visitor $visitor): void
+    {
+        $visitor->visitHouse($this);
+    }
 
 }
