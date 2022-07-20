391751b31012cc4b8139d8289cf49396b6e92a31
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-21 01:57:27 +0300

    Add visitHouse function to Securityclass

--- src/Behavior/Visitor/Security.php
+++ src/Behavior/Visitor/Security.php
@@ -4,5 +4,9 @@ namespace Patterns\Behavior\Visitor;
 
 class Security implements Visitor
 {
+    public function visitHouse(House $element): void
+    {
+        echo $element->offerForHouse() . " Security company<br />";
+    }
 
 }
