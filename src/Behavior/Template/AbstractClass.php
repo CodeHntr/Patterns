80ec0defd8e24f0e975005ea30dbfe1ab4f31228
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-21 01:07:05 +0300

    add templateMethod

--- src/Behavior/Template/AbstractClass.php
+++ src/Behavior/Template/AbstractClass.php
@@ -4,5 +4,15 @@ namespace Patterns\Behavior\Template;
 
 abstract class AbstractClass
 {
+    final public function templateMethod(): void
+    {
+        $this->baseOperation1();
+        $this->requiredOperations1();
+        $this->baseOperation2();
+        $this->hook1();
+        $this->requiredOperation2();
+        $this->baseOperation3();
+        $this->hook2();
+    }
 
 }
