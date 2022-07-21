4cdbe08586f136c216f4b7c75d94f85b35f8f037
CodeHntr <paveltrokhanovsky@gmail.com>
2022-07-21 13:23:42 +0300

    Add constructor in RedHeadDuck

--- src/Behavior/Strategy/RedHeadDuck.php
+++ src/Behavior/Strategy/RedHeadDuck.php
@@ -4,5 +4,10 @@ namespace Patterns\Behavior\Strategy;
 
 class RedHeadDuck extends Duck
 {
+    public function __construct($wings, $quack)
+    {
+        $this->setFly($wings);
 
+        $this->setQuack($quack);
+    }
 }
