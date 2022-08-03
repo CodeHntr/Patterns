d517a1f295f269852bc01ef2126c75d54fd2adbe
CodeHntr <paveltrokhanovsky@gmail.com>
2022-08-03 14:22:03 +0300

    Add to controller function showMessages

--- src/Architectural/MVC/Controller.php
+++ src/Architectural/MVC/Controller.php
@@ -5,5 +5,12 @@ namespace Patterns\Architectural\MVC;
 
 class Controller
 {
+    public function showMessages()
+    {
+        $model = new Model();
+        $messages = $model->getMessages();
+        $view = new View();
+        $view->listMessages($messages);
+    }
 
 }
