96daf00bffdc6ebe4892d65e2be00182dd56cd55
CodeHntr <paveltrokhanovsky@gmail.com>
2022-08-03 14:18:05 +0300

    add property and function addMessage

--- src/Architectural/MVC/Model.php
+++ src/Architectural/MVC/Model.php
@@ -5,5 +5,11 @@ namespace Patterns\Architectural\MVC;
 
 class Model
 {
+    private array $messages = [];
+
+    public function addMessage($message)
+    {
+        $this->messages[] = date("H:i:s") . " : " . $message;
+    }
 
 }
