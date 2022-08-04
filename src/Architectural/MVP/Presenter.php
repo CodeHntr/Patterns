bdc6b792b6954e5397c52b134b7bd2d34abe8899
CodeHntr <paveltrokhanovsky@gmail.com>
2022-08-04 17:14:08 +0300

    add properties and function construct

--- src/Architectural/MVP/Presenter.php
+++ src/Architectural/MVP/Presenter.php
@@ -4,6 +4,14 @@ namespace Patterns\Architectural\MVP;
 
 class Presenter
 {
+    private $model;
+    private $view;
+
+    public function __construct(Model $model, View $view)
+    {
+        $this->model = $model;
+        $this->view = $view;
+    }
 
 }
 
