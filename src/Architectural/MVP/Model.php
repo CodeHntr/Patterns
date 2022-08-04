6a5f8b99e8f5f3a216b8d00a2d8b2838d57708d4
CodeHntr <paveltrokhanovsky@gmail.com>
2022-08-04 17:11:44 +0300

    Add generate function and prop

--- src/Architectural/MVP/Model.php
+++ src/Architectural/MVP/Model.php
@@ -4,5 +4,12 @@ namespace Patterns\Architectural\MVP;
 
 class Model
 {
+    private int $number;
+
+    public function generateNum()
+    {
+        $this->number = mt_rand(mt_rand(0, 10), mt_rand(10, 20));
+
+    }
 
 }
