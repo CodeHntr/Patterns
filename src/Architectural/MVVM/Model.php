0931619e5ea1ebc03538d7134450f30c650da23a
CodeHntr <paveltrokhanovsky@gmail.com>
2022-09-01 17:20:49 +0300

    Add functions for Interface

--- src/Architectural/MVVM/Model.php
+++ src/Architectural/MVVM/Model.php
@@ -4,5 +4,11 @@ namespace Patterns\Architectural\MVVM;
 
 interface Model
 {
+    public function create(array $data);
 
+    public function update(int $state, int $id);
+
+    public function getById(int $id);
+
+    public function delete(int $id);
 }
