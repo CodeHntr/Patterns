38766f39134cbc930e77045ee205b7c553033845
CodeHntr <paveltrokhanovsky@gmail.com>
2022-09-01 17:23:23 +0300

    Add properties for TrafficLight

--- src/Architectural/MVVM/TrafficLight.php
+++ src/Architectural/MVVM/TrafficLight.php
@@ -7,6 +7,15 @@ use PDO;
 
 class TrafficLight implements Model
 {
+    public const STATE_GREEN = 1;
+    public const STATE_YELLOW = 2;
+    public const STATE_RED = 3;
+
+    private string $table = 'traffic_lights';
+    protected PDO $pdo;
+    public int $id;
+    public string $address;
+    public int $state;
 
 
 }
